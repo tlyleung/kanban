4e51a37bc4c5276d75007b0f4e7470b7846a6af3
Timothy Leung <timothyleung@gmail.com>
2025-01-15 17:49:09 +0800

    Add GitHub Actions workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    assetPrefix: '/kanban/',
+    basePath: '/kanban',
     eslint: { dirs: ['app', 'systems'] },
+    output: "export",
 };
 
 export default nextConfig;
